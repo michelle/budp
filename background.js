67a04cfbff18b4f78b7e011e268d864481b55286
Michelle Bu <analogmidnight@gmail.com>
2012-12-16 22:25:19 -0800

    arraybuf conversion success

--- background.js
+++ background.js
@@ -14,6 +14,17 @@ chrome.extension.onConnect.addListener(function(port) {
     port.postMessage({data: data});
   };
 
+  // Convert JSON to ArrayBuffer for sending UDP packets.
+  function json_to_ab(obj) {
+    str = JSON.stringify(obj);
+    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+    var buf_view = new Uint16Array(buf);
+    for (var i = 0, strlen = str.length; i < strlen; i += 1){
+      buf_view[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
   port.onMessage.addListener(function(msg, sender) {
     // Save tab associated with connection for future reference.
     var host_port = [msg.host, msg.port];
@@ -32,8 +43,7 @@ chrome.extension.onConnect.addListener(function(port) {
             chrome.extension.onConnect.addListener(function(data_port) {
               console.assert(data_port.name == 'budp-client');
               data_port.onMessage.addListener(function(msg) {
-                // TODO: convert data to ArrayBuffer
-                chrome.socket.write(socketId, msg.data, function() {
+                chrome.socket.write(socketId, json_to_ab(msg.data), function() {
                   console.log('Sent message: ', msg.data);
                 });
               });
@@ -46,8 +56,6 @@ chrome.extension.onConnect.addListener(function(port) {
       console.log('Connection already exists');
       // TODO: Error object passed back here.
     }
-
-    // Send the message.
 
   });
 });
